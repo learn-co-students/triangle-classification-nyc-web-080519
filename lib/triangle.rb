27711d166189d11472852369bef1701d404b4fd6
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-18 12:45:13 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_accessor :len1, :len2, :len3
+
+
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind
+    if len1 <= 0 || len2 <=0 || len3 <= 0
+      raise TriangleError
+    elsif len1 + len2 <= len3
+      raise TriangleError
+    elsif len1 + len3 <= len2
+      raise TriangleError
+    elsif len3 + len2 <= len1
+      raise TriangleError
+    elsif len1 == len2 && len2 == len3
+      return :equilateral
+    elsif
+      len1 == len2 || len1 == len3 || len2 == len3
+      return :isosceles
+    elsif len1 != len2 && len2 != len3 && len1 != len3
+      return :scalene
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+  end
+
 end
